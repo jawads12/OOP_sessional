75f899dca4e402e073000d9eac7b99d33b53d901
jawads12 <96069993+jawads12@users.noreply.github.com>
2022-04-18 15:00:51 +0600

    Create pratice.cpp

---
+++ 18.4.22/pratice.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class toyota
+{
+    string model;
+    int year;
+    string color;
+public:
+    void set_name(string name){
+    
+    }
+    
+    void set_year(int year){
+    
+    }
+    
+    void set_color(string color){
+    
+    }
+    
+    string get_name()
+    {
+    return name;
+        
+    }
+    
+    int get_year()
+    {
+    return year;
+        
+    }
+    
+    string get_color()
+    {
+    return color;
+        
+    }
+    
+    
+    
+    
+    void sameCar()
+    {
+        if()
+        
+        
+    }
+    
+    
+};
+
+
+int main ()
+{
+    toyota c1;
+    
+    return 0;
+}
